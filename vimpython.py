987a2a106f305eca998abc564945f826f3ba3dcc
neo <test@liu.can.com>
2015-01-14 15:58:54 +0800

    add python plugin test function

---
+++ vimpython.py
@@ -0,0 +1,17 @@
+import vim
+cb=vim.current.buffer
+'''i=len(cb)
+j=0
+while j<i:
+    cb[j]=cb[j]+"***"
+    j+=1
+'''
+j=0
+import sys
+for i in sys.stdin:
+#    cb[j]=i+"fdfdfdfdsafdaf"
+    j+=1
+    print "xxx"
+
+
+
